92b3e3a4802e1dfdf87e470ff801b694911f929f
KW <88893976+KdoubleUs@users.noreply.github.com>
2022-04-27 20:23:54 -0400

    fix bugs

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -11,11 +11,15 @@ const Container = styled.section`
   display: grid;
   grid-template-columns: 1fr 1fr;
   height: 100vh;
-  width: 80%;
+  width: 70%;
 `;
 const ColumnLeft = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  line-height: 1.8;
 `;
 const ColumnRight = styled.div`
   display: flex;
@@ -29,22 +33,26 @@ function AboutMe() {
           <h1>Who Am i? </h1>
           <div>
             <p>
-              {" "}
-              Hello everyone! My name is Kevin Wu and I am a Software developer
-              from New York City. I love to build projects for the webs. I
-              graduated from John Jay College with a degree in Forensic
-              Chemistry and minor in psychology. I transition to software
-              development when I was first exposed to HTML5 and CSS3. I
-              instantly fell in love with the freedom, creativity, and the
-              ability to control what i do and make. The inspiration and liberty
-              of building a website from scratch was so appealing to me and It's
-              because of this passion, I continue to love software development.
-              I continue to learn everyday and I enjoy every step of the journey
+              <span className="span-text">
+                Hello everyone! My name is Kevin Wu and I am a Software
+                developer from New York City.
+              </span>
+              I love to build projects for the webs. I graduated from John Jay
+              College with a degree in Forensic Chemistry and minor in
+              psychology. I transition to software development when I was first
+              exposed to HTML5 and CSS3. I instantly fell in love with the
+              freedom, creativity, and the ability to control what i do and
+              make.
+              <br></br>
+              The inspiration and liberty of building a website from scratch was
+              so appealing to me and It's because of this passion, I continue to
+              love software development. I continue to learn everyday and I
+              enjoy every step of the journey
             </p>
           </div>
         </ColumnLeft>
         <ColumnRight>
-          <h1>Filler</h1>
+          <h1>Insert Image</h1>
         </ColumnRight>
       </Container>
     </Section>
