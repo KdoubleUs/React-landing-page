a52278c3c36ceeb2521762adcd29923982a58da0
KW <88893976+KdoubleUs@users.noreply.github.com>
2022-04-27 22:08:35 -0400

    added tables

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -28,44 +28,57 @@ function Contact() {
           love to meet new people and make new friends! Lets Connect
         </p>
         <form className="contact-form">
-          <label className="label">
-            Name:
-            <motion.input
-              type="text"
-              placeholder="insert your name"
-              whileFocus={{ boxShadow: "3px 5px 0 rgba(255,245,10, 0.9)" }}
-              initial={{ opacity: 0, x: -1000 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.2 }}
-              className="inputs"
-            />
-          </label>
-          <label>
-            Email:
-            <motion.input
-              type="text"
-              placeholder="insert your email"
-              whileFocus={{ boxShadow: "3px 5px 0px rgba(255,245,10, 0.9)" }}
-              initial={{ opacity: 0, x: -1000 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.2 }}
-              className="inputs"
-            />
-          </label>
-          <label>
-            Message:
-            <motion.input
-              type="text"
-              whileFocus={{ boxShadow: "3px 5px 0px rgba(255,245,10, 0.9)" }}
-              initial={{ opacity: 0, x: -1000 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.2 }}
-              className="inputs"
-            />
-          </label>
-          <button type="submit" className="submitbtn">
-            Submit
-          </button>
+          <table>
+            <tr>
+              <td align="" className="label">
+                Name:
+                <motion.input
+                  type="text"
+                  placeholder="insert your name"
+                  whileFocus={{ boxShadow: "3px 5px 0 rgba(255,245,10, 0.9)" }}
+                  initial={{ opacity: 0, x: -1000 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  transition={{ duration: 0.2 }}
+                  className="inputs"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td align="" className="label">
+                Email:
+                <motion.input
+                  type="text"
+                  placeholder="insert your email"
+                  whileFocus={{
+                    boxShadow: "3px 5px 0px rgba(255,245,10, 0.9)",
+                  }}
+                  initial={{ opacity: 0, x: -1000 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  transition={{ duration: 0.2 }}
+                  className="inputs"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td align="" className="label">
+                Message:
+                <motion.input
+                  type="text"
+                  whileFocus={{
+                    boxShadow: "3px 5px 0px rgba(255,245,10, 0.9)",
+                  }}
+                  initial={{ opacity: 0, x: -1000 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  transition={{ duration: 0.2 }}
+                  className="inputs"
+                />
+              </td>
+            </tr>
+
+            <button type="submit" className="submitbtn">
+              Submit
+            </button>
+          </table>
         </form>
       </Container>
     </Section>
