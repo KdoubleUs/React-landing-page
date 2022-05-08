1d39352bed8a67607ce5bfc2b5330ef45266d7a8
KW <88893976+KdoubleUs@users.noreply.github.com>
2022-05-07 23:45:29 -0400

    added image to about me section

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import personalImg from "../images/personal-image.jpg";
 const Section = styled.section`
   height: 100vh;
   width: 100vw;
@@ -40,23 +41,25 @@ function AboutMe() {
               I love to build projects for the webs. I graduated from John Jay
               College with a degree in Forensic Chemistry and minor in
               psychology. I transition to software development when I was first
-              exposed to HTML5 and CSS3. I instantly fell in love with the
+              introuduce to HTML5 and CSS3. I instantly fell in love with the
               freedom, creativity, and the ability to control what i do and
-              make.
+              create. I knew right away that coding was a career path i want to
+              take. I love the complexity and challenges that coding present.
             </p>
             <p>
               The inspiration and liberty of building a website from scratch was
               so appealing to me and It's because of this passion, I continue to
-              love software development.{" "}
+              love software development.
               <span className="span-text">
                 I continue to learn everyday and I enjoy every step of the
-                journey{" "}
+                journey. I want to take my coding experiences and skills to new
+                heights.
               </span>
             </p>
           </div>
         </ColumnLeft>
         <ColumnRight>
-          <h1>Insert Image</h1>
+          <img src={personalImg} className="personal-img" alt="none" />
         </ColumnRight>
       </Container>
     </Section>
